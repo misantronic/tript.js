eaaea1679d1e4b55e8618a6c743c68a6c1c321c5
misantronic <david.skx@posteo.de>
2015-02-12 16:07:34 +0100

    added helper functionality

--- tript.js
+++ tript.js
@@ -11,13 +11,13 @@ function T(t, c, r, _) {
 	return eval(
 		// set context
 		"with(c||{}){_='';" +
-		// match html-tags
+			// match html-tags
 		t[r = "replace"](/(<\w+.*>)|(<\/\w+>)/g, function(p, a, b) {
 			return '_+="'+
 				(a || b)
 					// replace quotes
 					[r](/"/g, '\\"')
 					// inline javascript
-					[r](/<%(.*?)%>|\$(\w+(\.*\w+\([\s\S]*\))*)/g, '"+($1$2)+"') +'";'
-	}) + "}")
+					[r](/<%(.*?)%>|\$(\w+(?:\.*\w*)*)\|*([|\w]*)/g, '"+ ("$3" ? T["$3"]($1$2) : ($1$2)) +"') +'";'
+		}) + "}")
 }
